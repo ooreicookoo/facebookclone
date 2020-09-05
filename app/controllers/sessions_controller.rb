e496da0bfcc5a84cef08b5a962eb8d5af2f98bad
Reiko Enomoto <ooreicookoo@gmail.com>
2020-09-05 15:22:59 +0900

    WIP: flash message at application.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,11 @@
 class SessionsController < ApplicationController
   def new
   end
+  def Create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+  end
 end
