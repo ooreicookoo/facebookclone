0ff1965368c9e4ff19a62aa09cb9bb44ddc20437
Reiko Enomoto <ooreicookoo@gmail.com>
2020-09-05 16:55:06 +0900

    WIP:ブログ機能、コントローラ、モデル生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_014356) do
+ActiveRecord::Schema.define(version: 2020_09_05_075217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
