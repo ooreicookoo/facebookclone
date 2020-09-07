a0309627912803dc598592610796b152fa226145
Reiko Enomoto <ooreicookoo@gmail.com>
2020-09-07 22:22:55 +0900

    WIP:ルーティング にconfirmを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
-  get 'sessions/new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-  resources :blogs
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
 end
