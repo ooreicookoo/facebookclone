5f9b4a61d42379f300f05907a053986685a3cb82
Reiko Enomoto <ooreicookoo@gmail.com>
2020-09-07 15:03:00 +0900

    WIP:edit機能実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,11 +6,26 @@ class BlogsController < ApplicationController
     @blog = Blog.new
   end
   def create
-    Blog.create(blog_params)
-    redirect_to new_blog_path
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: '投稿しました！'
+    else
+      render :new
+    end
   end
   def show
     @blog = Blog.find(params[:id])
+  end
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "投稿を編集しました"
+    else
+      render :edit
+    end
   end
   private
   def blog_params
