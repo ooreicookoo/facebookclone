1393b4cf16860f3147d615fcfd20dd3dad05747b
Reiko Enomoto <ooreicookoo@gmail.com>
2020-09-14 11:19:38 +0900

    WIP　userとpicturesをモデルを関連付け

--- db/migrate/20200908231637_create_feeds.rb
+++ db/migrate/20200908231637_create_feeds.rb
@@ -3,6 +3,7 @@ class CreateFeeds < ActiveRecord::Migration[5.2]
     create_table :feeds do |t|
       t.text :image
       t.timestamps
+      t.references :user, foreign_key: true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2020_09_08_231637) do
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_feeds_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_09_08_231637) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "feeds", "users"
 end
