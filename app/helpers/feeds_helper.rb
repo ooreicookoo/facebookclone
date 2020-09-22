77e7f273960d9e385edabb3fc6bb1635e12bb379
Reiko Enomoto <ooreicookoo@gmail.com>
2020-09-23 08:04:21 +0900

    WIP　投稿編集機能実装

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,9 +1,9 @@
 module FeedsHelper
-  def confirm_new_or_edit
-    @feed.id ? feed_path : feeds_path
-  end
-
-  def confirm_form_method
-    @feed.id ? 'patch' : 'post'
-  end
+  def choose_new_or_edit
+     if action_name == 'new' || action_name == 'create'
+       confirm_feeds_path
+     elsif action_name == 'edit'
+       feed_path
+     end
+   end
 end
